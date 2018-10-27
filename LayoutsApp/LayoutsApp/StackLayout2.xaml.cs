ff5e95ae5ffb2abe64302b4bb10c9f81817bd8f7
Heba Mohammed <heba.odesk.tasks@gmail.com>
2018-10-27 23:07:24 +0200

    Add project files.

---
+++ LayoutsApp/LayoutsApp/StackLayout2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LayoutsApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class StackLayout2 : ContentPage
+	{
+		public StackLayout2 ()
+		{
+			InitializeComponent ();
+            var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/sports/7/") };
+            imageSource.CachingEnabled = false;
+            image.Source = imageSource;
+
+            
+		}
+
+	}
+}
