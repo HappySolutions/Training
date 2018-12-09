35dea1b815c45e04153cd94f0ab20beb0f700197
Heba Ahmed <heba.odesk.tasks@gmail.com>
2018-12-10 00:55:59 +0200

    the buttons

---
+++ SweetsDokkana/SweetsDokkana.Android/RoundedButtonRendererAndroid.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Android.Graphics.Drawables;
+using SweetsDokkana.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using SweetsDokkana.Styles;
+
+
+[assembly: ExportRenderer(typeof(RoundedButton), typeof(RoundedButtonRendererAndroid))]
+
+namespace SweetsDokkana.Droid
+{
+    public class RoundedButtonRendererAndroid : ButtonRenderer
+    {
+        private GradientDrawable _normal, _pressed;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+              Control.SetBackgroundResource(Resource.Layout.rounded_Button);
+            }
+        }
+
+        
+    }
+}
---
+++ SweetsDokkana/SweetsDokkana.Android/RoundedEntryRendererAndroid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics.Drawables;
+using SweetsDokkana.Styles;
+using SweetsDokkana.Droid;
+
+[assembly: ExportRenderer(typeof(RoundedEntry), typeof(RoundedEntryRendererAndroid))]
+namespace SweetsDokkana.Droid
+{
+    public class RoundedEntryRendererAndroid : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement == null)
+            {
+                Control.SetBackgroundResource(Resource.Layout.rounded_shape);
+               
+            }
+        }
+    }
+}
