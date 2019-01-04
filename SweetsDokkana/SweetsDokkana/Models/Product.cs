eeae977d9f91b838822a16c2e5db2bb898ecf4dc
Heba Ahmed <heba.odesk.tasks@gmail.com>
2019-01-04 18:11:13 +0200

    sqliteEdit

--- SweetsDokkana/SweetsDokkana/Models/Product.cs
+++ SweetsDokkana/SweetsDokkana/Models/Product.cs
@@ -7,9 +7,9 @@ namespace SweetsDokkana.Models
     public class Product
     {
         public int Id { get; set; }
-        public string brand { get; set; }
+        public string discreption { get; set; }
         public string name { get; set; }
-        public string price { get; set; }
+        public int price { get; set; }
         public string image_link { get; set; }
     }
 }
--- SweetsDokkana/SweetsDokkana/Views/OrientalProductsPage.xaml.cs
+++ SweetsDokkana/SweetsDokkana/Views/OrientalProductsPage.xaml.cs
@@ -13,15 +13,37 @@ namespace SweetsDokkana.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class OrientalProductsPage : ContentPage
 	{
-        private const string Url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
-        private HttpClient _client = new HttpClient();
-        private ObservableCollection<Product> _products;
+        //private const string Url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+       // private HttpClient _client = new HttpClient();
+        //private ObservableCollection<Product> _products;
 
         public OrientalProductsPage ()
 		{
 			InitializeComponent ();
-		}
-        protected override async void OnAppearing()
+
+            SweetsList.ItemsSource = new List<Product>
+            {
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=1080", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=999", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=995", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=835", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=889", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=882", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=824", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=772", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=755", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=674", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=493", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=488", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=429", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=431", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=360", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=326", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=312", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=306", discreption = "It is a long established fact that a reader will.", price = 15}
+            };
+        }
+        /*protected override async void OnAppearing()
         {
             var content = await _client.GetStringAsync(Url);
             var products = JsonConvert.DeserializeObject<List<Product>>(content);
@@ -31,19 +53,51 @@ namespace SweetsDokkana.Views
             booksListView.ItemsSource = _products;
 
             base.OnAppearing();
-        }
+        }*/
 
         private void listView_Refreshing(object sender, EventArgs e)
         {
-            booksListView.ItemsSource = _products;
+            SweetsList.ItemsSource = new List<Product>
+            {
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=1080", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=999", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=995", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=835", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=889", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=882", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=824", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=772", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=755", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=674", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=493", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=488", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=429", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=431", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=360", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=326", discreption = "It is a long established fact that a reader will.", price = 15},
+                new Product {name = "Lorem", image_link ="https://picsum.photos/200/300?image=312", discreption = "It is a long established fact that a reader will.", price = 12},
+                new Product {name = "Ibsom", image_link = "https://picsum.photos/200/300?image=306", discreption = "It is a long established fact that a reader will.", price = 15}
+            };
             //then we use this function to end the refreshing loading
-            booksListView.EndRefresh();
+            SweetsList.EndRefresh();
         }
+
         private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
-            booksListView.ItemsSource = e.NewTextValue;
+            SweetsList.ItemsSource = e.NewTextValue;
         }
-        async void OnAdd(object sender, System.EventArgs e)
+
+        private void AddToCart_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SweetsList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+
+        }
+
+        /*async void OnAdd(object sender, System.EventArgs e)
         {
             var product = new Product { name = "name " };
             _products.Insert(0, product);
@@ -52,15 +106,15 @@ namespace SweetsDokkana.Views
             var content = JsonConvert.SerializeObject(product);
 
             await _client.PostAsync(Url, new StringContent(content));
-        }
+        }*/
 
-        async void OnDelete(object sender, System.EventArgs e)
+        /*async void OnDelete(object sender, System.EventArgs e)
         {
             var product = _products[0];
             _products.Remove(product);
 
             await _client.DeleteAsync(Url + "/" + product.Id);
 
-        }
+        }*/
     }
 }
